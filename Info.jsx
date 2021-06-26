25eb6b9edd2b5d12433491294a8d89fb58b89258
sharanyapari <86248923+sharanyapari@users.noreply.github.com>
2021-06-26 18:14:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Web development BootCamp</h1>
+      <p>Starts on 27th july,basic of web development</p>
+    </div>
+  );
+}
+export default Info;
